cef4d6972efbee327321634f8ad6cba3228c10a8
Monika <maam092024@gmail.com>
2025-01-27 18:39:01 +0100

    logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Footer.css";
+import rijksLogo from "../assets/rijks.svg";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <img className="rijks-logo" src="rijks.svg" alt="Rijksmuseum logo" />
+      <img className="rijks-logo" src={rijksLogo} alt="Rijksmuseum logo" />
       <h4>
         Explore the rich art collection of Rijksmuseum and discover art history.
       </h4>
